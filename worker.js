ab37fcd0e06173e02ec4edc6af1155f989de99e8
AntonucciCooks <Mem.wdc@gmail.com>
2026-06-15 13:22:24 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 export default { 
-  async fetch(request, env, ctx) {
+  async fetch (request, env, ctx) {
     const bust = Date.now();
     const url = `https://raw.githubusercontent.com/AntonucciCooks/website/main/index.html?bust=${bust}`;
     const response = await fetch(url, { cache: 'no-store' });
